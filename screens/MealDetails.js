12c3ec36f774ce666570df5abc6aa407612ad76a
koleks92 <koleks92@gmail.com>
2023-09-18 18:28:02 +0200

    Custom button - Heart

--- screens/MealDetails.js
+++ screens/MealDetails.js
@@ -5,6 +5,7 @@ import Meal from '../models/meal';
 import MealExtra from '../components/MealExtra';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
+import IconButton from '../components/IconButton';
 
 
 function MealDetails ({ route, navigation }) {
@@ -18,7 +19,7 @@ function MealDetails ({ route, navigation }) {
         navigation.setOptions({
             title: item.title,
             headerRight: () => {
-                return <Button title="Here" onPress={headerButtorPressHandler}/>
+                return <IconButton onPress={ headerButtorPressHandler } icon="heart-outline" color="white"/>
             }
         });
     }, [item.title, navigation, headerButtorPressHandler]);
